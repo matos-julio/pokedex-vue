9d70e26211c6eb85089ae91252139c78516d0e9b
Julio Matos <73722761+matos-julio@users.noreply.github.com>
2025-04-08 22:51:29 -0300

    bootstrap instalado

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,11 @@
 </script>
 
 <template>
+  <div class="container mt-5">
+    <h1 class="text-primary">Teste do projeto</h1>
+    <button class="btn btn-success">Aperte aqui</button>
+  </div>
+
 </template>
 
 <style scoped>
