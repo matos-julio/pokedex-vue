108a6250f70be15f846da5c87717c1466f81fbac
Julio Matos <73722761+matos-julio@users.noreply.github.com>
2025-04-09 00:27:32 -0300

    fetch dos pokemons e exibicao inicial, atraves do app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,34 @@
-<script setup>
+<script>
+import { ref, onMounted } from 'vue';
+import { fetchPokemon, fetchPokemonDetails } from './services/api';
+
+export default {
+  setup() {
+    const pokemons = ref([]); // cria array vazia pra puxar os pokemon na api
+
+    onMounted(async () => {
+      const pokemonLista = await fetchPokemon(6)
+
+      const pokemonDetails = await Promise.all( // Promise.all = executa todas as promises ao mesmo tempo, e resolve
+        pokemonLista.map(p => fetchPokemonDetails(p.url)) // percorre cada item da lista, e faz fetch na url de cada pokemon (importante conferir o resultado da url)
+      )
+
+      pokemons.value = pokemonDetails
+    })
+
+    return {pokemons}
+  }
+}
+
 </script>
 
 <template>
-  <div class="container mt-5">
-    <h1 class="text-primary">Teste do projeto</h1>
-    <button class="btn btn-success">Aperte aqui</button>
-  </div>
+  <h1>Pokedex</h1>
+
+  <ul>
+    <li v-for="pokemon in pokemons" :key="pokemon.id"><p>{{ pokemon.name }}</p></li>
+  </ul>
 
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+
+// funcao pra puxar todos os pokemon da api
+export async function fetchPokemon(limit= 6) { //define quantos pokemon a api vai puxar | 6 = padrao
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`)
+  const data = await response.json()  
+  return data.results // objeto contendo cada pokemon {name, url}
+}
+
+export async function fetchPokemonDetails(url) {
+  const response = await fetch(url) // a url do objeto do fetch anterior
+  const data = await response.json()
+  return data // retorna todos os dados do pokemon
+}
